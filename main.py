699634bc69004206d377a0b88701fe9a1eca4a8c
em4nuelvt <emanuelvieiratavaresvt@gmail.com>
2023-10-23 12:02:00 -0300

    adicionando arquivo de potencia

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from lexical import LexicalAnalysis,TokenType,tt2str
 from syntatic import SyntaticAnalysis
 
 if __name__ == '__main__':
-    lex = LexicalAnalysis('exemplos/pow.tiny')
+    lex = LexicalAnalysis('exemplos/potencia.tiny')
     syntactic = SyntaticAnalysis(lex)
     cmd = syntactic.start()
     cmd.execute()
